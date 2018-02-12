f21ef3e3af0eebd8b104a790aa1fd6b33bbae325
Jesús Cueto <DMSoftware@github.com>
2018-02-12 00:00:46 -0500

    Fix

--- iCatchUp/ViewController/HomeNewsViewController.swift
+++ iCatchUp/ViewController/HomeNewsViewController.swift
@@ -35,9 +35,9 @@ class HomeNewsViewController: UICollectionViewController {
     
     // MARK: - Navigation
 
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+   /* override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         <#code#>
-    }
+    }*/
     /*// In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using [segue destinationViewController].
